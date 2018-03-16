64cac6c479e03b7e82792277115d397ea7cd6363
Baptiste Vrignaud <vrignaudbaptiste@gmail.com>
2018-03-16 20:25:33 +0100

    style: more styling + jquery

---
+++ src/main/resources/static/js/marquee.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+    var marquee = $('div.marquee');
+    console.log(marquee);
+    marquee.each(function() {
+        var mar = $(this),indent = mar.width();
+        mar.marquee = function() {
+            indent--;
+            mar.css('text-indent',indent);
+            if (indent < -1 * mar.children('div.marquee-text').width()) {
+                indent = mar.width();
+            }
+        };
+        mar.data('interval',setInterval(mar.marquee,1000/120));
+    });
+});
