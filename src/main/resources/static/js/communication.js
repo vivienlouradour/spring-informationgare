7786ec505694fd7d683ba55a3f0de80f36140c2b
Baptiste <vrignaudbaptiste@gmail.com>
2018-03-18 09:52:03 +0100

    L'Ange Slider

--- src/main/resources/static/js/communication.js
+++ src/main/resources/static/js/communication.js
@@ -1,13 +1,12 @@
-$(document).ready(function() {
-	
-	var refreshInterval = setInterval(ajaxInterval(), 10000);
+$(document).ready(function () {
 
     var currentLocation = window.location.pathname;
     var currentLocationSplit = currentLocation.split('/');
     var type = currentLocationSplit[1];
     var gare = currentLocationSplit[2];
+    var nbRows = $('#container').find('tr').length;
 
-    $("button").click(function(e) {
+    $("button").click(function (e) {
         console.log("BOUTON CLIQUE");
         e.preventDefault();
         $.ajax({
@@ -15,14 +14,40 @@ $(document).ready(function() {
             url: "/testAdd"
         });
     });
-    
-    function ajaxInterval() {
-    	
-    	$('#resultsList').load('/update',{type:type, gare:gare});
+
+    function resetScroll(){
+        clearInterval(scrollThread);
+        var delayScroll = 10000 + 2* 200 * nbRows;
+        console.log(nbRows);
+        console.log(delayScroll);
+        scrollThread = setInterval(scrollDownAndUp, delayScroll);
+
     }
-    
-    setInterval(function() {
-    	clearInterval(refreshInterval);
-    	slider();
-    }, 10000);
+
+    function scrollDownAndUp() {
+        if ($('#container').height() > $(window).height() - 70) {
+            var nbRows = $('#container').find('tr').length;
+            var time = 200 * nbRows;
+            console.log(nbRows);
+            $('body,html').animate({scrollTop: $(document).height()-$(window).height()}, time).delay(2000);
+            $('body,html').animate({scrollTop: 0}, time).delay(10000);
+        }
+    }
+
+    var scrollThread = null;
+    resetScroll();
+
+    setInterval(function(){
+        $('#container').load('/update',{type:type, gare:gare},
+            function (responseText, textStatus, XMLHttpRequest) {
+                if (textStatus == "success") {
+
+                }
+            });
+        if(nbRows < $('#container').find('tr').length){
+            nbRows = $('#container').find('tr').length;
+            resetScroll();
+        }
+    },1000);
+
 });
